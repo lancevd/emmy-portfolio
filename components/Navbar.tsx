d0da28b57c4aa441451fc4a3c915bd33fb7f0705
lancevd <olamosob@gmail.com>
2024-06-25 16:47:59 +0100

    Nav added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const menu = ([] = [
   {
-    name: "home",
+    name: "Home",
     link: "/",
   },
   {
@@ -22,10 +22,10 @@ const menu = ([] = [
 
 const Navbar = () => {
   return (
-    <nav className="border bg-[#E0E0E080] p-3 flex gap-3">
+    <nav className="border w-1/3 mx-auto rounded-[2rem] justify-evenly items-center bg-[#282828] border-[#E0E0E080] p-3 flex gap-3">
       {menu.map((item, index) => {
         return (
-          <Link href={item.link} key={index}>
+          <Link href={item.link} key={index} className="py-1 ">
             {item.name}
           </Link>
         );
