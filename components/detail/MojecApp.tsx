311aa51144af8ec360a680aa21369a16198b9d19
lancevd <olamosob@gmail.com>
2024-07-18 07:24:41 +0100

    .

--- components/detail/MojecApp.tsx
+++ components/detail/MojecApp.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const MojecApp: React.FC = () => {
   return (
-    <main>
+    <section>
         <p>Mojec Metering App</p>
         <h2 className="text-white">Solve all your electricity problem in one app</h2>
-    </main>
+    </section>
   )
 }
 
